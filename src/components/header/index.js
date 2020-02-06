6e37913e745cb402b91ee8dc15ed8368756d7c58
sssgordon <gso7@uwo.ca>
2020-02-06 18:52:34 +0100

    Header cross button

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header(props) {
+    return (
+        <div className="header">
+            <div className="header__button">
+                <a href="#">
+                    <div className="header__button-1"></div>
+                    <div className="header__button-2"></div>
+                </a>
+            </div>
+        </div>
+    );
+}
